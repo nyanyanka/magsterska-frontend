99d2240cc63df48fa1805b4d5b01aa7308916ddd
nyanyanka <emberplugin@mail.ru>
2019-11-05 20:02:32 +0500

    Flexberry code generation

---
+++ app/locales/ru/translations.js
@@ -0,0 +1,95 @@
+import Ember from 'ember';
+import EmberFlexberryTranslations from 'ember-flexberry/locales/ru/translations';
+
+
+
+const translations = {};
+Ember.$.extend(true, translations, EmberFlexberryTranslations);
+
+Ember.$.extend(true, translations, {
+  models: {
+,
+  },
+
+  'application-name': '',
+
+  forms: {
+    loading: {
+      'spinner-caption': 'Данные загружаются, пожалуйста подождите...'
+    },
+    index: {
+      greeting: 'Добро пожаловать на тестовый стенд ember-flexberry!'
+    },
+
+    application: {
+      header: {
+        menu: {
+          'sitemap-button': {
+            caption: '',
+            title: 'Меню'
+          },
+          'user-settings-service-checkbox': {
+            caption: 'Использовать сервис сохранения пользовательских настроек'
+          },
+          'show-menu': {
+            caption: 'Показать меню'
+          },
+          'hide-menu': {
+            caption: 'Скрыть меню'
+          },
+          'language-dropdown': {
+            caption: 'Язык приложения',
+            placeholder: 'Выберите язык'
+          }
+        },
+        login: {
+          caption: 'Вход'
+        },
+        logout: {
+          caption: 'Выход'
+        }
+      },
+
+      footer: {
+        'application-name': '',
+        'application-version': {
+          caption: 'Версия аддона {{version}}',
+          title: 'Это версия аддона ember-flexberry, которая сейчас используется в этом тестовом приложении ' +
+          '(версия npm-пакета + хэш коммита). ' +
+          'Кликните, чтобы перейти на GitHub.'
+        }
+      },
+
+      sitemap: {
+        'application-name': {
+          caption: '',
+          title: ''
+        },
+        'application-version': {
+          caption: 'Версия аддона {{version}}',
+          title: 'Это версия аддона ember-flexberry, которая сейчас используется в этом тестовом приложении ' +
+          '(версия npm-пакета + хэш коммита). ' +
+          'Кликните, чтобы перейти на GitHub.'
+        },
+        index: {
+          caption: 'Главная',
+          title: ''
+        },
+,
+      }
+    },
+
+    'edit-form': {
+      'save-success-message-caption': 'Сохранение завершилось успешно',
+      'save-success-message': 'Объект сохранен',
+      'save-error-message-caption': 'Ошибка сохранения',
+      'delete-success-message-caption': 'Удаление завершилось успешно',
+      'delete-success-message': 'Объект удален',
+      'delete-error-message-caption': 'Ошибка удаления'
+    },
+,
+  },
+
+});
+
+export default translations;
